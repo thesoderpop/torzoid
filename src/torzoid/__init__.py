de6ee8f7db55c8ccf42525c9ac53e99505cee4b3
thesoderpop <zipzy.xyz@gmail.com>
2026-04-12 15:54:24 -0400

    Add package init

---
+++ src/torzoid/__init__.py
@@ -0,0 +1,9 @@
+"""Torzoid: temporal tiny-pointer memory prototype."""
+
+from .store import ObjectRecord, StoreStats, TemporalTinyPointerStore
+
+__all__ = [
+    "ObjectRecord",
+    "StoreStats",
+    "TemporalTinyPointerStore",
+]
